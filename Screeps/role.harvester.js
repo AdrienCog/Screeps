47e91626873388ed14211c35ac0f6188a8e22e8b
Adrien Cognee <adrien@zenchef.com>
2017-04-18 18:37:29 +0200

    Find nearest

--- Screeps/role.harvester.js
+++ Screeps/role.harvester.js
@@ -3,9 +3,9 @@ var roleHarvester = {
     /** @param {Creep} creep **/
     run: function(creep) {
         if(creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#ffaa00'}});
+            var sources = creep.pos.findNearest(Game.SOURCES):
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
         else {
